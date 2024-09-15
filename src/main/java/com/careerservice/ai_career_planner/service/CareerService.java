f047f3a7ca090eb2eb4588088bbcbe3be1761b8c
Taewan Kim <paladyn12@naver.com>
2024-09-15 13:56:16 +0900

    Commit

--- src/main/java/com/careerservice/ai_career_planner/service/CareerService.java
+++ src/main/java/com/careerservice/ai_career_planner/service/CareerService.java
@@ -23,6 +23,7 @@ public class CareerService {
 
     public void saveTestResult(String interests, String personalities, String subjects, String hollandCode) {
 
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String loginId = auth.getName();
 
